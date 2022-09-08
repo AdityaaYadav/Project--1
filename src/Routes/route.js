be878b92dcf12e4776379f531d72228ae3664628
AdityaaYadav <yadavadityaa007@gmail.com>
2022-09-08 16:38:44 +0530

    project-1

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -16,6 +16,7 @@ router.delete("/blogs/:blogId",tokenverify, auth, blogcontroller.deleteblog)
 
 router.delete("/blogs", tokenverify, auth, blogcontroller.deletebolgbyquery)
 
+router.post("/login",tokenverify, autherController.loginAuther)
 
 
 module.exports = router;
